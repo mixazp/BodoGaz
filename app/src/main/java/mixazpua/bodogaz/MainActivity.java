8f0a652a51b0662c52902754c99ad4a9635f9cb3
vasja vasja <vasja vasja>
2016-08-11 20:58:38 +0300

    Initial commit

---
+++ app/src/main/java/mixazpua/bodogaz/MainActivity.java
@@ -0,0 +1,38 @@
+package mixazpua.bodogaz;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TabHost;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        setTitle("TabHost");
+
+        TabHost tabHost = (TabHost) findViewById(R.id.tabHost);
+
+        tabHost.setup();
+
+        TabHost.TabSpec tabSpec = tabHost.newTabSpec("tag1");
+
+        tabSpec.setContent(R.id.linearLayout);
+        tabSpec.setIndicator("Кот");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag2");
+        tabSpec.setContent(R.id.linearLayout2);
+        tabSpec.setIndicator("Кошка");
+        tabHost.addTab(tabSpec);
+
+        tabSpec = tabHost.newTabSpec("tag3");
+        tabSpec.setContent(R.id.linearLayout3);
+        tabSpec.setIndicator("Котёнок");
+        tabHost.addTab(tabSpec);
+
+        tabHost.setCurrentTab(0);
+    }
+}
